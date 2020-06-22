b17507303ad6141ebaa18f012b300bd547c71eca
nawodawi <nawodawijebandara7@gmail.com>
2020-06-22 11:53:47 +0530

    php file updated

--- app.php
+++ app.php
@@ -1,7 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <body>
-        
+
+
+   
  
 <?php
 
@@ -20,28 +19,40 @@ $channel_link = $channel->getElementsByTagName('link')
 ->item(0)->childNodes->item(0)->nodeValue;
 $channel_desc = $channel->getElementsByTagName('description')
 ->item(0)->childNodes->item(0)->nodeValue;
+$channel_date = $channel->getElementsByTagName('lastBuildDate')
+->item(0)->childNodes->item(0)->nodeValue;
 
 //output elements from "<channel>"
 echo("<p><a href='" . $channel_link
   . "'>" . $channel_title . "</a>");
 echo("<br>");
 echo($channel_desc . "</p>");
+echo("Last updated at: ");
+echo(  $channel_date);
 
 //get and output "<item>" elements
 $x=$xmlDoc->getElementsByTagName('item');
-for ($i=0; $i<=5; $i++) {
+for ($i=0; $i<=7; $i++) {
   $item_title=$x->item($i)->getElementsByTagName('title')
   ->item(0)->childNodes->item(0)->nodeValue;
   $item_link=$x->item($i)->getElementsByTagName('link')
   ->item(0)->childNodes->item(0)->nodeValue;
   $item_desc=$x->item($i)->getElementsByTagName('description')
   ->item(0)->childNodes->item(0)->nodeValue;
+  $item_date=$x->item($i)->getElementsByTagName('pubDate')
+  ->item(0)->childNodes->item(0)->nodeValue;
+  
   echo ("<p><a href='" . $item_link
-  . "'>" . $item_title . "</a>");
+  . "'>" ."<h3>". $item_title."</h3>" . "</a>");
   echo ("<br>");
   echo ($item_desc . "</p>");
-}
+  echo ("Published at: ". $item_date);
+  echo ("<hr style=\"height:4px;border-width:0;color:gray;background-color:gray\">");
+ 
+ }
+
 ?>
 
-</body>
-</html>
+
+
+
